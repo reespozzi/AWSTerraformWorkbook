de1d2acfcac3dd87bdb9841b90842318dcc1d565
Rees Pozzi <rees.pozzi@kainos.com>
2021-10-13 23:16:07 +0100

    Added very simple web server to run on the Ubuntu machine

--- main.tf
+++ main.tf
@@ -2,11 +2,31 @@ provider "aws" {
     region="eu-west-2"
 }
 
-resource "aws_instance" "FirstServer"{
-    ami           ="ami-095379b4e8d257b76"
-    instance_type ="t2.micro"
+resource "aws_security_group" "sg"{
+    name            ="FirstServerSG"
 
-    tags          ={
-        Name      ="Built in Terraform"
+    ingress{
+        from_port   =8080
+        to_port     =8080
+        protocol    ="tcp"
+        #CIDR block specifies IP ranges, 0.0.0.0/0 covers all IP addresses
+        cidr_blocks =["0.0.0.0/0"]
+    }
+}
+
+resource "aws_instance" "FirstServer"{
+    ami                     ="ami-095379b4e8d257b76"
+    instance_type           ="t2.micro"
+    #resource attribute references inside []
+    vpc_security_group_ids  =[aws_security_group.sg.id]
+    #<<-EOF and EOF allow multiline strings without newline characters
+    user_data               = <<-EOF
+                            #!/bin/bash
+                            echo "Hello there!" > index.html
+                            nohup busybox httpd -f -p 8080 &
+                            EOF
+
+    tags                    ={
+        Name                ="Built in Terraform"
     }
 }
